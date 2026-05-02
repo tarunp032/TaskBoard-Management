722c1f7aef693f477abfb60bd62707577b7c1bf8
tarunp032 <prajapattarun7568@gmail.com>
2026-05-02 13:14:17 +0530

    TASKBOARD

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,8 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail({ to, subject, html }) {
   try {
     const data = await resend.emails.send({
-      from: "onboarding@resend.dev", 
-      to,
+      from: "TaskBoard <noreply@prajapattarun.site>", 
+      to, 
       subject,
       html,
     });
